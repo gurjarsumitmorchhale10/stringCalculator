ff82c685aeb850219986384a2f925ec0ab9ef292
gurjarsumitmorchhale10 <gurjarsumitmorchhale10@gmail.com>
2024-06-10 16:43:51 +0530

    Made code fix to handle two or more arguments

--- src/main/java/com/codewithme/stringcalculator/StringCalculator.java
+++ src/main/java/com/codewithme/stringcalculator/StringCalculator.java
@@ -1,16 +1,25 @@
 package com.codewithme.stringcalculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
 
     public int add(String str) {
         int sum = 0;
         if (str == null || str.isEmpty() || str.isBlank()) {
-            return sum;
+            return 0;
         }
         else
         {
-            sum=Integer.parseInt(str);
+            sum=extractNumbers(str).stream().reduce(sum,Integer::sum);
         }
+
         return sum;
+    }
+
+    private List<Integer> extractNumbers(String str) {
+        return Arrays.stream(str.split(",")).mapToInt(Integer::parseInt).boxed().toList();
     }
 }
