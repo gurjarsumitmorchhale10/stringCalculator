83b36f6a758b9792b6e47290671c6509c76f55ae
gurjarsumitmorchhale10 <gurjarsumitmorchhale10@gmail.com>
2024-06-10 16:16:33 +0530

    fixed code for empty argument

--- src/main/java/com/codewithme/stringcalculator/StringCalculator.java
+++ src/main/java/com/codewithme/stringcalculator/StringCalculator.java
@@ -3,5 +3,11 @@ package com.codewithme.stringcalculator;
 public class StringCalculator {
 
     public int add(String str) {
+        int sum = 0;
+        if (str == null || str.isEmpty() || str.isBlank()) {
+            return sum;
+        }
+
+        return sum;
     }
 }
